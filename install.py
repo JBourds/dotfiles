fdc7187f9cc3727ff711241252af834e20618218
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-02-26 00:16:22 -0500

    LSP fixes.

--- install.py
+++ install.py
@@ -3,7 +3,6 @@ import subprocess
 import sys
 from typing import List
 
-
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 USER = os.getlogin()
 HOME_DIR = os.path.expanduser(f"~{USER}")
@@ -17,12 +16,13 @@ if os.geteuid() != 0:
     print("Error: File must be run with superuser privileges", file=sys.stderr)
     exit(1)
 
+
 def install_if_not_installed(program: str, install_cmds: List[str]) -> bool:
     result = subprocess.run(
-            ["which", program],
-            capture_output=True,
-            text=True,
-        )
+        ["which", program],
+        capture_output=True,
+        text=True,
+    )
     if result.returncode == 0:
         print(f"Found {program} installation at {result.stdout}")
         return True
@@ -45,6 +45,7 @@ def install_if_not_installed(program: str, install_cmds: List[str]) -> bool:
         print(f"Failed to install `{program}`")
     return success
 
+
 def copy_dotfiles() -> bool:
     if not install_if_not_installed("stow", ["sudo apt install stow"]):
         print("Failed to install GNU Stow to setup symlinks")
@@ -53,7 +54,7 @@ def copy_dotfiles() -> bool:
     # Change to the script directory
     cwd = os.getcwd()
     os.chdir(SCRIPT_DIR)
-    
+
     if not DOTFILE_DIRS:
         print("No dotfile directories found.")
         return False
@@ -61,15 +62,17 @@ def copy_dotfiles() -> bool:
         print(f"Stowing the following packages: {DOTFILE_DIRS}")
 
     cmd = f"stow --target={HOME_DIR} " + ' '.join(DOTFILE_DIRS)
-    result = subprocess.run(cmd, shell=True, stderr=sys.stderr, stdout=sys.stdout)
+    result = subprocess.run(
+        cmd, shell=True, stderr=sys.stderr, stdout=sys.stdout)
     os.chdir(cwd)
 
     if result.returncode == 0:
         print("Successfully set up dotfile symlinks")
     else:
         print("Failed to set up dotfile symlinks")
-    
+
     return result.returncode == 0
+
 
 if __name__ == "__main__":
     copy_dotfiles()
@@ -85,4 +88,3 @@ if __name__ == "__main__":
         "which bat",
     ])
     install_if_not_installed("rg", ["sudo apt install ripgrep"])
-
