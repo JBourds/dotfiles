a1ff2fd060bb87d40025e9e5128df544cf0d2e02
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-03-03 11:29:32 -0500

    Adding config for fish shell.

---
+++ .config/.config/fish/config.fish
@@ -0,0 +1,16 @@
+if type -q nvim
+    alias vim "nvim"
+end
+if type -q rg
+    alias grep "rg"
+end
+if type -q bat
+    alias cat "bat"
+end
+
+alias c "clear"
+alias ls 'ls --color=auto'
+alias ll 'ls -alF'
+alias la 'ls -A'
+alias grep "grep --color=auto"
+alias diff "diff --color=auto"
