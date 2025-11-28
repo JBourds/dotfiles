ddd39c5057dc9eaa3dc0431f95f57f0d066ea7fb
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-11-27 23:00:17 -0500

    wezterm: Setting default background.

--- .config/.config/wezterm/wezterm.lua
+++ .config/.config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 require("wezterm")
-local config = {}
-config.enable_wayland = false
-config.warn_about_missing_glyphs = false
-
-return config
+return {
+    enable_wayland = false,
+    warn_about_missing_glyphs = false,
+    color_scheme = 'Kanagawa (Gogh)',
+}
